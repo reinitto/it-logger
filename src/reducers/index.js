4931f21669494ce03b11d2a78378d866c994acb1
Labais <reinitto@gmail.com>
2019-06-20 15:03:52 +0200

    finished project

---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import logReducer from './logReducer';
+import techReducer from './techReducer';
+export default combineReducers({ log: logReducer, tech: techReducer });
