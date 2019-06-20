a68e971e66f0bddc9b6b0131861a72d76964784a
Labais <reinitto@gmail.com>
2019-06-20 17:10:18 +0200

    delete build folder

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use('/techs', require('./routes/techs'));
 app.use('/logs', require('./routes/logs'));
 
 //Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('build'));
+// if (process.env.NODE_ENV === 'production') {
+//   //Set static folder
+//   app.use(express.static('build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  );
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+//   );
+// }
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
