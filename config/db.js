e124cf0a3d9bb5a2eb4dda86987c6a2040ab6a5e
Labais <reinitto@gmail.com>
2020-05-17 21:51:56 +0200

    add netlify react app env var

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const db = process.env.MONGO_URI;
+const db = process.env.MONGO_URI || process.env.REACT_APP_MONGO_URI;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
     });
 
-    console.log('MongoDB Connected...');
+    console.log("MongoDB Connected...");
   } catch (error) {
     console.error(error.message);
     process.exit(1);
