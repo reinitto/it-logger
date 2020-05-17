34632aaf8193476480ffd23d0a7ccb973c7bacc3
Labais <reinitto@gmail.com>
2020-05-17 22:02:00 +0200

    add res logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -28,6 +28,8 @@ export const getLogs = () => async (dispatch) => {
     setLoading();
     const res = await fetch("/logs");
     const data = await res.json();
+    console.log("res", res);
+    console.log("data", data);
     dispatch({
       type: GET_LOGS,
       payload: data,
--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -12,6 +12,8 @@ export const getTechs = () => async (dispatch) => {
     setLoading();
     const res = await fetch("/techs");
     const data = await res.json();
+    console.log("res", res);
+    console.log("data", data);
     dispatch({
       type: GET_TECHS,
       payload: data,
