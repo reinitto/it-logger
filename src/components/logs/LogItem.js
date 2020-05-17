edbba7ef06c51f8113449f0a7a6a89e42ace4da3
Labais <reinitto@gmail.com>
2020-05-17 21:14:02 +0200

    fix log item

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { deleteLog, setCurrent } from '../../actions/logActions';
-import Moment from 'react-moment';
-import PropTypes from 'prop-types';
-import M from 'materialize-css/dist/js/materialize.min';
+import React from "react";
+import { connect } from "react-redux";
+import { deleteLog, setCurrent } from "../../actions/logActions";
+import Moment from "react-moment";
+import PropTypes from "prop-types";
+import M from "materialize-css/dist/js/materialize.min";
 
 const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
     deleteLog(log._id);
-    M.toast({ html: 'Log Deleted' });
+    M.toast({ html: "Log Deleted" });
   };
 
   return (
-    <li className='collection-item'>
+    <li
+      className="collection-item"
+      draggable="true"
+      onDrag={() => console.log("dragging")}
+    >
       <div>
         <a
-          href='#edit-log-modal'
+          href="#edit-log-modal"
           className={`modal-trigger ${
-            log.attention ? 'red-text' : 'blue-text'
+            log.attention ? "red-text" : "blue-text"
           }`}
           onClick={() => setCurrent(log)}
         >
           {log.message}
         </a>
         <br />
-        <span className='grey-text'>
-          <span className='black-text'>ID #{log._id}</span> last updated by{' '}
-          <span className='black-text'>{log.tech}</span> on{' '}
-          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+        <span className="grey-text">
+          <span className="black-text">ID #{log._id}</span> last updated by{" "}
+          <span className="black-text">{log.tech}</span> on{" "}
+          <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
-        <a href='#!' onClick={onDelete} className='secondary-content'>
-          <i className='material-icons grey-text'>delete</i>
+        <a href="#!" onClick={onDelete} className="secondary-content">
+          <i className="material-icons grey-text">delete</i>
         </a>
       </div>
     </li>
@@ -43,7 +47,4 @@ LogItem.propTypes = {
   setCurrent: PropTypes.func.isRequired
 };
 
-export default connect(
-  null,
-  { deleteLog, setCurrent }
-)(LogItem);
+export default connect(null, { deleteLog, setCurrent })(LogItem);
